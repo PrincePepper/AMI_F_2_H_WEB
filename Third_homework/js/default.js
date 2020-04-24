647d9b04f65c00c514780a7665421d7fd110cc37
PrincePepper <semyon3336@gmail.com>
2020-04-24 14:03:09 +1000

    3 домашка

--- Third_homework/js/default.js
+++ Third_homework/js/default.js
@@ -1,22 +1,3 @@
-$(function () {
-    $(".owl-carousel").owlCarousel({
-        items: 1,
-        merge: true,
-        loop: true,
-        margin: 10,
-        video: true,
-        lazyLoad: true,
-        center: true,
-        responsive: {
-            320: {
-                items: 1
-            },
-            560: {
-                items: 2
-            },
-            992: {
-                items: 4
-            }
-        }
-    });
+$(window).on("load", function () {
+    $(".loader-container").fadeOut(1000);
 });
