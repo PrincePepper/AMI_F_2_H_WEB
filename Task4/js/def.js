86c44dbdd942527b584546a7118e1dd49bd743b4
PrincePepper <semyon3336@gmail.com>
2020-04-30 21:04:04 +1000

    update task4

--- Task4/js/def.js
+++ Task4/js/def.js
@@ -6,16 +6,26 @@ function fib(n) {
     return n <= 1 ? n : fib(n - 1) + fib(n - 2);
 }
 
+function getRandomInt(min, max) {
+    // случайное число от min до (max+1)
+    let rand = min + Math.random() * (max + 1 - min);
+    return Math.floor(rand);
+}
+
+
 $('#inputraduis').focus();
 
 $("form").submit(function () {
     let c = $('#inputraduis')
     let f = $('#inputfib')
-    let i = $('#inputmin')
-    let x = $('#inputmax')
+    let i2 = $('#inputminrandom')
+    let x = $('#inputmaxrandom')
 
     let c_v = c.val();
     let f_v = f.val();
+    let i_v = i2.val();
+    let x_v = x.val();
+
 
     const cir1 = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     cir1.setAttribute("cx", "500");
@@ -48,5 +58,24 @@ $("form").submit(function () {
         $('#answer-fib').text(fib(f_v));
     } else $('#answer-fib').text(fib(f.attr('placeholder')));
 
+
+    let table = $('<table id="tabb"/>')
+    let columnsCount = 8;
+    let rowsCount = 5;
+    for (let i = 0; i < rowsCount; i++) {
+        let row = $('<tr/>')
+        for (let j = 0; j < columnsCount; j++) {
+            let cell = $('<td/>')
+            if (i_v) {
+                cell.html(getRandomInt(parseInt(i_v), parseInt(x_v)))
+
+            } else {
+                cell.html(getRandomInt(parseInt(i2.attr('placeholder')), parseInt(x.attr('placeholder'))))
+            }
+
+        }
+        table.append(row)
+    }
+    table.appendTo('#table')
 
 });
